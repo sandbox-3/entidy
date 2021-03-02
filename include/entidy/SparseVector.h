21c7f1a5de53bd1fc369e1bb522c82ed251c098a
Francois Zard <francoiszard@gmail.com>
2021-03-02 19:26:27 +0200

    added/edited documentation

--- include/entidy/SparseVector.h
+++ include/entidy/SparseVector.h
@@ -33,6 +33,10 @@ protected:
 	MemoryManager memory_manager;
 	size_t size = 0;
 
+	/**
+     * @brief Returns a recycled or created page from the memory pool.
+     * @return A pointer to the created page.
+     */
 	Page<PageSize>* Pop()
 	{
 		Page<PageSize>* page = memory_manager->Pop<Page<PageSize>>();
@@ -46,6 +50,9 @@ public:
 		this->memory_manager = memory_manager;
 	}
 
+	/**
+     * @brief Returns all the pages to the memory pool for recycling.
+     */
 	~SparseVectorImpl()
 	{
 		for(size_t i = 0; i < pages.size(); i++)
@@ -55,6 +62,10 @@ public:
 		}
 	}
 
+	/**
+     * @brief Returns value at selected index. If page does not exist, returns 0
+     * @return Value at selected index or 0 if empty.
+     */
 	intptr_t Read(size_t index) const
 	{
 		size_t page_index = std::floor(index / PageSize);
@@ -67,6 +78,11 @@ public:
 		return pages[page_index]->data[block_index];
 	}
 
+	/**
+     * @brief Writes or replaces value 'value' at index 'index'. Creates page if it doesn't exist.
+     * If 'value' is 0, no page is created.
+     * @return Previous value if cell at 'index' was not 0.
+     */
 	intptr_t Write(size_t index, intptr_t value)
 	{
 		if(value == 0)
@@ -89,10 +105,15 @@ public:
 			pages[page_index]->count++;
 			++size;
 		}
-        
-        return prev;
+
+		return prev;
 	}
 
+	/**
+     * @brief Sets value at index 'index' to 0.
+     * If page is empty after erasure, sends page back to memory pool for recycling.
+     * @return Previous value at 'index'.
+     */
 	intptr_t Erase(size_t index)
 	{
 		size_t page_index = std::floor(index / PageSize);
@@ -121,6 +142,10 @@ public:
 		return prev;
 	}
 
+	/**
+     * @brief Returns total number of non-zero items in SparseVector.
+     * @return Total number of non-zero items in SparseVector.
+     */
 	size_t Size() const
 	{
 		return size;

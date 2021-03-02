3fc2021e058ea8c29c0fa6fabef522c9a56bedcc
Francois Zard <francoiszard@gmail.com>
2021-03-02 18:34:19 +0200

    added documentation

--- include/entidy/Entidy.h
+++ include/entidy/Entidy.h
@@ -22,31 +22,55 @@ class Entidy
 protected:
 	Indexer indexer;
 
-
 public:
 	Entidy()
 	{
 		indexer = make_shared<IndexerImpl>();
 	}
 
-	~Entidy()
-	{
-	}
+	~Entidy() { }
 
+	/**
+     * @brief Returns a new or recycled entity.
+     * @return Entity.
+     */
 	Entity Create()
 	{
 		return indexer->AddEntity();
 	}
 
+	/**
+     * @brief Creates, indexes and returns a memory-managed instance of component.
+     * The new instance is allocated or recycled by the memory pool.
+     * If the component key does not exist, it is created and a Type association is saved.
+     * @tparam Type The component type.
+     * @param entity The entity.
+     * @param key The key for for the component to add.
+     * @param args... The arguments to forward to Type's constructor.
+     * @return A pointer to the created component.
+     * @throw EntidyException if the key had been previously used for a different type.
+     */
 	template <typename Type, typename... Args>
 	Type* Emplace(Entity entity, const string& key, Args&&... args)
 	{
 		Type* c = indexer->CreateComponent<Type>(entity, key);
 		new(c) Type(std::forward<Args>(args)...);
-        
 		return c;
 	}
 
+	/**
+     * @brief Creates, indexes and returns a memory-managed instance of component.
+     * The new instance is allocated or recycled by the memory pool.
+     * If the component key does not exist, it is created and a Type association is saved.
+     * The provided component will be copied into the newly created component.
+     * WARNING: The provided component to copy must NOT contain non memory-managed heap-allocated data. 
+     * @tparam Type The component type.
+     * @param entity The entity.
+     * @param key The key for for the component to add.
+     * @param component The component that will be copied into the newly created component.
+     * @return A pointer to the created component.
+     * @throw EntidyException if the key had been previously used for a different type.
+     */
 	template <typename Type>
 	Type* Emplace(Entity entity, const string& key, const Type& component)
 	{
@@ -56,36 +80,74 @@ public:
 		return c;
 	}
 
-	bool Erase(Entity entity, const string& key)
-	{
-		intptr_t prev = indexer->DeleteComponent(entity, key);
-		return prev != 0;
-	}
-
+	/**
+     * @brief Removes an entity and deletes all its components.
+     * Sends all the deleted components to the memory-manager for recycling.
+     * @param entity The entity to remove.
+     */
 	void Erase(Entity entity)
 	{
 		indexer->RemoveEntity(entity);
 	}
 
+	/**
+     * @brief Deletes component with key 'key' for entity 'entity'.
+     * Also sends the instance back to the memory-manager for recycling.
+     * @param entity The entity.
+     * @param key The key for for the component to delete.
+     * @return false if component was not found, true otherwise.
+     */
+	bool Erase(Entity entity, const string& key)
+	{
+		return indexer->DeleteComponent(entity, key);
+	}
+
+	/**
+     * @brief Checks if Entity 'entity' has a component with key 'key'.
+     * @param entity The entity.
+     * @param key The key for for the component to check.
+     * @return false if component was not found, true otherwise.
+     */
 	bool Has(Entity entity, const string& key)
 	{
 		return indexer->HasComponent(entity, key);
 	}
 
+	/**
+     * @brief Returns a Query object that is pre-built with a list of component keys to fetch.
+     * @param keys A list of keys to fetch. Empty lists are allowed.
+     * @return A Query object with the list of requested components.
+     */
 	Query Select(const initializer_list<string>& keys)
 	{
 		return Query(indexer, keys);
 	}
 
+	/**
+     * @brief Returns a pointer to the component with key 'key' for entity 'entity'.
+     * NULL values are possible if 'entity' does not have a component for 'key'
+     * @tparam Type The component type.
+     * @param entity The entity.
+     * @param key The key for for the requested component.
+     * @return A pointer of type 'Type' to the component at 'key'.
+     * @throw EntidyException if the provided Type does not match with the type associated with 'key'.
+     */
 	template <typename Type>
 	Type* Component(Entity entity, const string& key)
 	{
 		return indexer->GetComponent<Type>(entity, key);
 	}
 
+	/**
+     * @brief Sets a hint that helps the memory-manager decide on optimal sizes for its memory blocks.
+     * This should be called BEFORE the first instance of component 'key' has been emplaced.
+     * There are no guarantees that the hint will be honored.
+     * @param key The key for for the requested component.
+     * @param size_hint A hint of the maximum number of components with key 'key' to be be expected.
+     */
 	void SizeHint(const string& key, size_t size_hint)
 	{
-        // TODO : Size Hints
+		// TODO : Size Hints
 	}
 
 	void CleanUp()
--- include/entidy/Indexer.h
+++ include/entidy/Indexer.h
@@ -1,7 +1,7 @@
 #pragma once
+#include <typeinfo>
 #include <unordered_map>
 #include <vector>
-#include <typeinfo>
 
 #include <entidy/CRoaring/roaring.hh>
 #include <entidy/Exception.h>
@@ -24,7 +24,7 @@ struct ComponentMap
 	BitMap entities;
 	SparseVector<DEFAULT_SV_SIZE> components;
 	MemoryManager mem_pool;
-    size_t type;
+	size_t type;
 };
 
 class IndexerImpl;
@@ -45,6 +45,12 @@ protected:
 
 	MemoryManager sv_mem_pool;
 
+	/**
+     * @brief Creates a new component with key 'key'.
+     * This function also handles recycling component indices.
+     * @param key The key for the requested component.
+     * @return The index of the newly created component.
+     */
 	size_t NewComponent(const string& key)
 	{
 		size_t c;
@@ -64,6 +70,12 @@ protected:
 		return c;
 	}
 
+	/**
+     * @brief Returns the index of the component with key 'key'.
+     * If the component does not exist, it is created.
+     * @param key The key for the requested component.
+     * @return The index of the component.
+     */
 	size_t ComponentIndex(const string& key)
 	{
 		auto it = index.find(key);
@@ -79,18 +91,10 @@ public:
 		sv_mem_pool = MemoryManagerImpl::Create<Page<DEFAULT_SV_SIZE>>();
 	}
 
-	bool HasEntity(Entity e)
-	{
-		if(entityRefCount <= e)
-			return false;
-
-		for(auto& it : entity_pool)
-			if(it == e)
-				return false;
-
-		return true;
-	}
-
+	/**
+     * @brief Returns a new or recycled entity.
+     * @return Entity.
+     */
 	Entity AddEntity()
 	{
 		Entity entity;
@@ -106,6 +110,10 @@ public:
 		return entity;
 	}
 
+	/**
+     * @brief Removes an entity and deletes all its components.
+     * @param entity The entity to remove.
+     */
 	void RemoveEntity(Entity entity)
 	{
 		for(auto& map : maps)
@@ -119,74 +127,93 @@ public:
 		entity_pool.push_back(entity);
 	}
 
-	bool HasComponent(Entity e, const string& key)
+	/**
+     * @brief Checks if entity 'entity' has a component at key 'key'.
+     * @param entity The entity to check.
+     * @param key The key for the component to check.
+     */
+	bool HasComponent(Entity entity, const string& key)
 	{
 		size_t c = ComponentIndex(key);
-		return maps[c].entities.contains(e);
+		return maps[c].entities.contains(entity);
 	}
 
+	/**
+     * @brief Creates, indexes and returns a memory-managed instance of component.
+     * The new instance is allocated or recycled by the memory pool.
+     * If the component key does not exist, it is created and a Type association is saved.
+     * @tparam Type The component type.
+     * @param entity The entity.
+     * @param key The key for for the component to add.
+     * @return A pointer to the created component.
+     * @throw EntidyException if the key had been previously used for a different type.
+     */
 	template <typename Type>
 	Type* CreateComponent(Entity entity, const string& key)
 	{
 		size_t c = ComponentIndex(key);
 
-        if(!maps[c].mem_pool)
-            maps[c].mem_pool = MemoryManagerImpl::Create<Type>();
-        
-        if(maps[c].type == 0)
-            maps[c].type = typeid(Type*).hash_code();
-        
-        if(maps[c].type != typeid(Type*).hash_code())
-            throw(EntidyException("Component Type mismatch for key " + key));
+		if(!maps[c].mem_pool)
+			maps[c].mem_pool = MemoryManagerImpl::Create<Type>();
+
+		if(maps[c].type == 0)
+			maps[c].type = typeid(Type*).hash_code();
+
+		if(maps[c].type != typeid(Type*).hash_code())
+			throw(EntidyException("Component Type mismatch for key " + key));
 
 		intptr_t prev = maps[c].components->Read(entity);
 		if(prev != 0)
 			maps[c].mem_pool->Push(prev);
 
-        Type * cur = maps[c].mem_pool->Pop<Type>();
+		Type* cur = maps[c].mem_pool->Pop<Type>();
 		maps[c].components->Write(entity, (intptr_t)cur);
 		maps[c].entities.add(entity);
 		return cur;
 	}
 
-	intptr_t DeleteComponent(Entity entity, const string& key)
+	/**
+     * @brief Deletes component with key 'key' for entity 'entity'.
+     * Also sends the instance back to the memory-manager for recycling.
+     * @param entity The entity.
+     * @param key The key for for the component to delete.
+     * @return false if component was not found, true otherwise.
+     */
+	bool DeleteComponent(Entity entity, const string& key)
 	{
 		size_t c = ComponentIndex(key);
 		maps[c].entities.remove(entity);
 		intptr_t prev = maps[c].components->Erase(entity);
 		if(prev != 0 && maps[c].mem_pool)
 			maps[c].mem_pool->Push(prev);
-		return prev;
+		return prev != 0;
 	}
 
+	/**
+     * @brief Returns a pointer to the component with key 'key' for entity 'entity'.
+     * NULL values are possible if 'entity' does not have a component for 'key'
+     * @tparam Type The component type.
+     * @param entity The entity.
+     * @param key The key for for the requested component.
+     * @return A pointer of type 'Type' to the component at 'key'.
+     * @throw EntidyException if the provided Type does not match with the type associated with 'key'.
+     */
 	template <typename Type>
 	Type* GetComponent(Entity entity, const string& key)
 	{
 		size_t c = ComponentIndex(key);
-        if(maps[c].type != typeid(Type*).hash_code())
-            throw(EntidyException("Component Type mismatch for key " + key));
+		if(maps[c].type != typeid(Type*).hash_code())
+			throw(EntidyException("Component Type mismatch for key " + key));
 		return (Type*)maps[c].components->Read(entity);
 	}
 
-	void CleanUp()
-	{
-		auto it = index.begin();
-		while(it != index.end())
-		{
-			auto map = maps[it->second];
-			if(map.entities.cardinality() == 0)
-			{
-				component_pool.push_back(it->second);
-				it = index.erase(it);
-			}
-			else
-			{
-				++it;
-			}
-			map.entities.shrinkToFit();
-		}
-	}
-
+	/**
+     * @brief Performs a query and copies lists of pointers to the requested components.
+     * @param keys The ordered list of components requested. Empty list is allowed.
+     * @param filter A query string to filter by.
+     * @return A View with lists of pointers to the requested components.
+     * @throw EntidyException if the filter string has a syntax error or is empty.
+     */
 	View Fetch(const vector<string>& keys, const string& filter)
 	{
 		QueryParser<BitMap> qp(this);
@@ -212,15 +239,15 @@ public:
 			++it;
 		}
 
-        vector<size_t> types(keys.size()+1);
-        types[0] = typeid(Entity).hash_code();
+		vector<size_t> types(keys.size() + 1);
+		types[0] = typeid(Entity).hash_code();
 		for(size_t k = 0; k < keys.size(); k++)
 		{
 			i = 0;
 			size_t c = ComponentIndex(keys[k]);
 
-            SparseVector<DEFAULT_SV_SIZE> sv = maps[c].components;
-            types[k + 1] = maps[c].type;
+			SparseVector<DEFAULT_SV_SIZE> sv = maps[c].components;
+			types[k + 1] = maps[c].type;
 
 			it = query.begin();
 
@@ -235,8 +262,34 @@ public:
 		return View(results, types);
 	}
 
-	// Query Parser Adapter Functions
+	/**
+     * @brief Removes empty component pools and deallocates their reserved memory.
+     * Removes orphaned entities that have no components attached to them.
+     * Optimizes the bitmaps for faster queries and reduced memory consumption.
+     * Should be called infrequently, and only when too many temporary dynamic components were created and deleted.
+     */
+	void CleanUp()
+	{
+		auto it = index.begin();
+		while(it != index.end())
+		{
+			auto map = maps[it->second];
+			if(map.entities.cardinality() == 0)
+			{
+				component_pool.push_back(it->second);
+				it = index.erase(it);
+			}
+			else
+			{
+				++it;
+			}
+			map.entities.shrinkToFit();
+		}
 
+		// TODO: Remove orphaned entities
+	}
+
+	// Query Parser Adapter Functions
 	virtual BitMap Evaluate(const string& token) override
 	{
 		size_t id = ComponentIndex(token);
@@ -256,7 +309,7 @@ public:
 	virtual BitMap Not(const BitMap& rhs) override
 	{
 		auto copy = BitMap(rhs);
-		copy.flip(0, copy.maximum());
+		copy.flip(0, componentRefCount);
 		return copy;
 	}
 };
